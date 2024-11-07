b9c3aee9009429688365e671101f86aec41bded2
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2024-11-07 19:05:26 -0400

    permitiendo cors de cualquier dominio

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración temporal de CORS
+  app.enableCors(); // Permite cualquier origen temporalmente
+
   await app.listen(3001);
 }
 bootstrap();
