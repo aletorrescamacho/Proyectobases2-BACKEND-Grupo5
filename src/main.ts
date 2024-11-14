e7b96dbfe96321fa4734b26cbc26e815ee31046e
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2024-11-13 20:15:45 -0400

    sigo con cookies

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,14 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 3600000, // 1 hora en milisegundos
-        httpOnly: true,
+        httpOnly: false,
         secure: false, 
         sameSite: 'None' // Esto permite las cookies entre diferentes orígenes
       }
     })
   );
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://proyectobases2-backend-grupo5-production.up.railway.app/'], // Agrega aquí el dominio de tu frontend
+    origin: true, // Agrega aquí el dominio de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true // Habilita las cookies si estás usando sesiones
   });
