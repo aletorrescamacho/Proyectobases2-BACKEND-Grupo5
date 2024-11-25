5c19e75683225bee5e865b217a0fa8709936e635
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2024-11-25 17:06:29 -0400

    agregando vercel en el cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://proyectobases2-backend-grupo5-production.up.railway.app'], // Agrega aquí el dominio de tu frontend
+    origin: ['http://localhost:3000', 'https://proyectobases2-backend-grupo5-production.up.railway.app', 'https://proyectobases2-frontend-grupo5-momgq5w95.vercel.app/','https://proyectobases2-frontend-grupo5.vercel.app/'], // Agrega aquí el dominio de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false // Habilita las cookies si estás usando sesiones
   });
